edc1e24c5419d495cc9fe4c5a23a46e397f32f91
adleramar <adlerarielmartinez@gmail.com>
2022-12-06 21:37:09 -0600

    Added CoreData support

--- PokeApp/PokemonViewModel.swift
+++ PokeApp/PokemonViewModel.swift
@@ -10,17 +10,29 @@ import Apollo
 import PokemonAPI
 
 class PokemonViewModel {
+    var allPokemon: [PokemonDetails]
+    
     init() {
-        
+        allPokemon = []
     }
+    
     func fetchDataFromService() {
-        ApolloNetworkHelper.shared.apolloClient.fetch(query: GetAllPokemonQuery()) { result in
+        ApolloNetworkHelper.shared.apolloClient.fetch(query: GetAllPokemonQuery()) { [weak self] result in
             switch result {
             case .success(let petitionResult):
-                print("Success! Result: \(petitionResult)")
+                guard let pokemon = petitionResult.data?.getAllPokemon else {
+                    print("Error: \(String(describing: petitionResult.errors))")
+                    return
+                }
+//                print("Success! Result: \(petitionResult)")
+                self?.allPokemon = self?.processPokemon(data: pokemon) ?? []
             case .failure(let error):
                 print("Failure! Error: \(error)")
             }
         }
+    }
+    
+    func processPokemon(data: [PokemonData]) -> [PokemonDetails] {
+        return Pokemon(data).pokemonList
     }
 }
